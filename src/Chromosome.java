8e63cd26fb34a3456f24167111223c9c4542a0e5
Neil <butcherna@appstate.edu>
2013-04-22 12:29:25 -0300

    Create Chromosome.java

---
+++ src/Chromosome.java
@@ -0,0 +1,24 @@
+public class Chromosome<T>
+{
+    DataType data;
+    public Chromosome(DataType d)
+    {
+     data = d;
+
+    }
+    public Chromosome()
+    {
+    }
+    public void hillClimb()
+    {
+        data.hillClimb();
+    }
+    public void setData(DataType d)
+    {
+        data = d;
+    }
+    public DataType getData()
+    {
+        return data;
+    }
+}
